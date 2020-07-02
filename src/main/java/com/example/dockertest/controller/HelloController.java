52aaf0f993171999d9c9a8f070f38545ac15bf55
敖杰 <jie.ao@hand-china.com>
2020-07-02 17:03:23 +0800

    修改返回消息

--- src/main/java/com/example/dockertest/controller/HelloController.java
+++ src/main/java/com/example/dockertest/controller/HelloController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
-        return "HELLO WORLD";
+        return "HELLO WORLD111";
     }
 }
