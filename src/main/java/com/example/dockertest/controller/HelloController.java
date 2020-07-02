d1c1a6c8e2908aa4fa4a28037c95d0ebc4a43390
敖杰 <jie.ao@hand-china.com>
2020-07-02 15:29:17 +0800

    初始化项目完成

---
+++ src/main/java/com/example/dockertest/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.dockertest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: docker-test
+ * @description: 查询controoler
+ * @packagename: com.example.dockertest.controller
+ * @author: jie.ao@hand-china.com
+ * @date: 2020-07-02 15:23
+ **/
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
